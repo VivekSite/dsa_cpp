895e3794412df02dd70fc76dd7ba0cf3464c1a47
VivekSite <sram39266@gmail.com>
2024-08-14 10:14:53 +0530

    Added a function for converting infix to postfix expression

--- Concepts/expressions.cpp
+++ Concepts/expressions.cpp
@@ -3,6 +3,26 @@
 class Expression_Function
 {
 private:
+  int precedence(char c)
+  {
+    if (c == '^')
+    {
+      return 3;
+    }
+    else if (c == '*' || c == '/')
+    {
+      return 2;
+    }
+    else if (c == '+' || c == '-')
+    {
+      return 1;
+    }
+    else
+    {
+      return -1;
+    }
+  }
+
 public:
   int evaluate_prefix(string str)
   {
@@ -85,13 +105,63 @@ public:
 
     return st.top();
   }
+
+  string infix_to_postfix(string str)
+  {
+    stack<char> st;
+    string res;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+      if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+      {
+        res += str[i];
+      }
+      else if (str[i] == '(')
+      {
+        st.push('(');
+      }
+      else if (str[i] == ')')
+      {
+        while (!st.empty() && st.top() != '(')
+        {
+          res += st.top();
+          st.pop();
+        }
+
+        if (!st.empty())
+        {
+          st.pop();
+        }
+      }
+      else
+      {
+        while (!st.empty() && precedence(st.top()) > precedence(str[i]))
+        {
+          res += st.top();
+          st.pop();
+        }
+        st.push(str[i]);
+      }
+    }
+
+    while (!st.empty())
+    {
+      res += st.top();
+      st.pop();
+    }
+    return res;
+  }
+
+  
 };
 
 int main()
 {
   Expression_Function ef;
-  cout << "Prefix Result: " << ef.evaluate_prefix("-+7*45+20") << ", Expected: 25" << lb;
-  cout << "Postfix Result: " << ef.evaluate_postfix("46+2/5*7+")<< ", Expected: 32" << lb;
+  // cout << "Prefix Result: " << ef.evaluate_prefix("-+7*45+20") << ", Expected: 25" << lb;
+  // cout << "Postfix Result: " << ef.evaluate_postfix("46+2/5*7+") << ", Expected: 32" << lb;
+  cout << "Infix To Postfix Result: " << ef.infix_to_postfix("(a-b/c)*(a/k-l)") << ", Expected: abc/-ak/l-*" << lb;
 
   return 0;
 }
