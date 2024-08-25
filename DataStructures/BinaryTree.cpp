7e1c724e30285b81e6da91d115852d0bb75ec835
VivekSite <sram39266@gmail.com>
2024-08-25 18:52:59 +0530

    Implemented a function to print levels of tree

--- DataStructures/BinaryTree.cpp
+++ DataStructures/BinaryTree.cpp
@@ -12,78 +12,115 @@ struct Node {
   }
 };
 
-void preorder_print(struct Node* root) {
-  if (root == NULL) return;
+class Print {
+public:
+  void preorder(struct Node* root) {
+    if (root == NULL) return;
 
-  cout << root->data << " ";
-  preorder_print(root->left);
-  preorder_print(root->right);
-}
-
-void inorder_print(struct Node* root) {
-  if (root == NULL) return;
-
-  inorder_print(root->left);
-  cout << root->data << " ";
-  inorder_print(root->right);
-}
-
-void postorder_print(struct Node* root) {
-  if (root == NULL) return;
-
-  postorder_print(root->left);
-  postorder_print(root->right);
-  cout << root->data << " ";
-}
-
-int search(int inorder[], int start, int end, int curr) {
-  for (int i=start; i<=end; i++) {
-    if (inorder[i] ==  curr) return i;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
   }
 
-  return -1;
-}
+  void inorder(struct Node* root) {
+    if (root == NULL) return;
 
-Node* build_tree_from_preorder(int preorder[], int inorder[], int start, int end) {
-  static int idx = 0;
-  if (start > end) return NULL;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+  }
 
-  int curr = preorder[idx];
-  idx++;
-  Node* node = new Node(curr);
-  if (start == end) return node;
+  void postorder(struct Node* root) {
+    if (root == NULL) return;
 
-  int pos = search(inorder, start, end, curr);
-  node->left = build_tree_from_preorder(preorder, inorder, start, pos-1);
-  node->right = build_tree_from_preorder(preorder, inorder, pos+1, end);
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+  }
 
-  return node;
-}
+  void levels(struct Node* root) {
+    if (root == NULL) return;
 
-Node* build_tree_from_postorder(int postorder[], int inorder[], int start, int end) {
-  static int idx = 4;
-  if (start > end) return NULL;
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
 
-  int curr = postorder[idx];
-  idx--;
-  Node* node = new Node(curr);
-  if (start == end) return node;
+    while (!q.empty()) {
+      Node* node = q.front();
+      q.pop();
+      
+      if (node != NULL) {
+        cout << node->data << " ";
+        
+        if (node->left) q.push(node->left);
+        if (node->right) q.push(node->right);
+      } else if (!q.empty()) {
+        q.push(NULL);
+      }
+    }
+  }
 
-  int pos = search(inorder, start, end, curr);
-  node->right = build_tree_from_postorder(postorder, inorder, pos+1, end);
-  node->left = build_tree_from_postorder(postorder, inorder, start, pos-1);
+};
 
-  return node;
-}
+class Tree {
+private:
+  int search(int inorder[], int start, int end, int curr) {
+    for (int i=start; i<=end; i++) {
+      if (inorder[i] ==  curr) return i;
+    }
+
+    return -1;
+  }
+
+public:
+  Node* from_preorder(int preorder[], int inorder[], int start, int end) {
+    static int idx = 0;
+    if (start > end) return NULL;
+
+    int curr = preorder[idx];
+    idx++;
+    Node* node = new Node(curr);
+    if (start == end) return node;
+
+    int pos = search(inorder, start, end, curr);
+    node->left = from_preorder(preorder, inorder, start, pos-1);
+    node->right = from_preorder(preorder, inorder, pos+1, end);
+
+    return node;
+  }
+
+  Node* from_postorder(int postorder[], int inorder[], int start, int end) {
+    static int idx = 4;
+    if (start > end) return NULL;
+
+    int curr = postorder[idx];
+    idx--;
+    Node* node = new Node(curr);
+    if (start == end) return node;
+
+    int pos = search(inorder, start, end, curr);
+    node->right = from_postorder(postorder, inorder, pos+1, end);
+    node->left = from_postorder(postorder, inorder, start, pos-1);
+
+    return node;
+  }
+
+
+};
 
 int main () {
-  int postorder[] = { 4, 2, 5, 3, 1 };
-  int preorder[] = { 1, 2, 4, 3, 5 };
-  int inorder[] = { 4, 2, 1, 5, 3 };
+  Print p;
+  Tree t;
 
-  Node* root = build_tree_from_postorder(preorder, inorder, 0, 4);
-  inorder_print(root);
-  cout << lb;
+  Node* root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+  root->left->left = new Node(4);
+  root->left->right = new Node(5);
+  root->right->left = new Node(6);
+  root->right->right = new Node(7);
+
+  p.levels(root);
 
   return 0;
 }
