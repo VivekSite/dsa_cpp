167343c18fa22ef9daa913f5772e5bac0fdf2b53
VivekSite <sram39266@gmail.com>
2024-08-25 17:48:10 +0530

    Implemented a BuildTree function to build a Tree from postorder and inorder tree

--- DataStructures/BinaryTree.cpp
+++ DataStructures/BinaryTree.cpp
@@ -44,7 +44,7 @@ int search(int inorder[], int start, int end, int curr) {
   return -1;
 }
 
-Node* buildTree(int preorder[], int inorder[], int start, int end) {
+Node* build_tree_from_preorder(int preorder[], int inorder[], int start, int end) {
   static int idx = 0;
   if (start > end) return NULL;
 
@@ -54,17 +54,34 @@ Node* buildTree(int preorder[], int inorder[], int start, int end) {
   if (start == end) return node;
 
   int pos = search(inorder, start, end, curr);
-  node->left = buildTree(preorder, inorder, start, pos-1);
-  node->right = buildTree(preorder, inorder, pos+1, end);
+  node->left = build_tree_from_preorder(preorder, inorder, start, pos-1);
+  node->right = build_tree_from_preorder(preorder, inorder, pos+1, end);
+
+  return node;
+}
+
+Node* build_tree_from_postorder(int postorder[], int inorder[], int start, int end) {
+  static int idx = 4;
+  if (start > end) return NULL;
+
+  int curr = postorder[idx];
+  idx--;
+  Node* node = new Node(curr);
+  if (start == end) return node;
+
+  int pos = search(inorder, start, end, curr);
+  node->right = build_tree_from_postorder(postorder, inorder, pos+1, end);
+  node->left = build_tree_from_postorder(postorder, inorder, start, pos-1);
 
   return node;
 }
 
 int main () {
+  int postorder[] = { 4, 2, 5, 3, 1 };
   int preorder[] = { 1, 2, 4, 3, 5 };
   int inorder[] = { 4, 2, 1, 5, 3 };
 
-  Node* root = buildTree(preorder, inorder, 0, 4);
+  Node* root = build_tree_from_postorder(preorder, inorder, 0, 4);
   inorder_print(root);
   cout << lb;
 
